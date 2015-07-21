4b86a016a36779f7509b471defc7d3cf1ab34f47
Chris Jackson <cjackson290@email.itt-tech.edu>
2015-07-20 20:46:33 -0500

    Part 2

---
+++ application.js
@@ -0,0 +1,14 @@
+function printHello() {
+	var christopher = document.getElementById("output");
+	christopher.innerHTML = "";
+	for(var i = 0; i<100; i++){
+	christopher.innerHTML += "<div>Hello world</div>";
+	}
+}
+function printGoodbye() {
+	var christopher = document.getElementById("output");
+	christopher.innerHTML = "";
+	for(var i = 0; i<100; i++){
+	christopher.innerHTML += "<div>Goodbye world</div>";
+	}
+}
